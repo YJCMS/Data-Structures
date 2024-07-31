98d12e983abb6a23aca3dd48837a4553259940cb
YJCMS <sen9563@gmail.com>
2024-07-31 07:15:59 +0000

    Q3

--- Binary_Tree/Q3_E_BT.c
+++ Binary_Tree/Q3_E_BT.c
@@ -102,6 +102,21 @@ int countOneChildNodes(BTNode *node)
 
 {
     /* add your code here */
+
+    if(node == NULL) {
+        return 0;
+    }
+
+    int count = 0;
+    if((node->left != NULL && node -> right == NULL) ||
+            node->left == NULL && node -> right != NULL) {
+        return count += 1;
+    }
+
+    count += countOneChildNodes(node->left);
+    count += countOneChildNodes(node->right);
+
+    return count;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
